ca09c2e85eb61a26e1195835b82bc321aff1ba22
ferdie <ferdiebergado@gmail.com>
2022-11-11 23:06:53 +0800

    Enabled autopairs' ( after function

--- lua/config/cmp.lua
+++ lua/config/cmp.lua
@@ -76,6 +76,7 @@ cmp.setup({
 })
 
 -- If you want insert `(` after select function or method item
---local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 --local cmp = require('cmp')
 --cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done({ map_char = { tex = "" } }))
